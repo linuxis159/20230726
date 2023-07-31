af55c2af0aa6ab3a6c0dce766ddf2c2d87ee1182
Ikgyeong.kim <dlrrud159@gmail.com>
2023-07-31 22:38:18 +0900

    add main page

--- src/main/java/com/spring/controller/Main.java
+++ src/main/java/com/spring/controller/Main.java
@@ -2,12 +2,19 @@ package com.spring.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Main {
     @RequestMapping("/")
     public String main(){
+        return "main";
+    }
+    @GetMapping("/search")
+    public String search(@RequestParam("searchWord") String query)
+    {
         return "main";
     }
 }
