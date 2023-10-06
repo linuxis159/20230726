8938f67d04363528258a9fccbcb48e48e1bdd090
Ikgyeong.kim <dlrrud159@naver.com>
2023-10-07 00:26:59 +0900

    using bootstrap

--- frontend/front_react/src/Header.js
+++ frontend/front_react/src/Header.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import './Header.css';
 
+import {Form, ListGroup} from "react-bootstrap";
 
 const Header = () => {
     const [query, setQuery] = useState('');
@@ -12,25 +13,42 @@ const Header = () => {
         const data = await response.json();
         setResults(data);
     }
+
+    const handleClick = (text) => {
+        setQuery(text);
+    };
     useEffect(() => {
         if (query) { // query가 비어있지 않을 때만 검색을 실행합니다.
             handlerSearch();
         }
     }, [query]);
     return(
+
         <div className='header'>
-            <input type="text" value={query} onChange={e => {
+{/*            <input type="text" value={query} onChange={e => {
                 setQuery(e.target.value);
             }
-            } />
+            } />*/}
+                <Form.Control
+                    type="text"
+                    value={query}
+                    onChange={e => {
+                        setQuery(e.target.value);
+                    }}
+                    placeholder="Start typing..."
+                />
             <button type="submit">Search</button>
-            {results.length > 0 && (
-                <ul>
-                    {results.map((result, index) => (
-                        <li key={index}>{result}</li>
-                    ))}
-                </ul>
-            )}
+                {results.length > 0 && (
+                    <ListGroup>
+                        {results.map((item, index) => (
+                            <ListGroup.Item key={index} onClick={() => handleClick(item)}>
+                                {item}</ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                )}
+
+
+
         </div>
     )
 }
